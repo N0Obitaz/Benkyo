3d38df70efcf9f67e0db77687db5c31692c3d474
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-01-03 23:44:18 +0800

    Added Scoped Service for Firebase

---
+++ Benkyo/Services/FirebaseService.cs
@@ -0,0 +1,16 @@
+﻿using Google.Cloud.Firestore;
+
+namespace Benkyo.Services
+{
+    public class FirebaseService
+    {
+
+        private readonly FirestoreDb _db;
+
+        public FirebaseService(FirestoreDb db)
+        {
+            _db = db;
+        }
+
+    }
+}
