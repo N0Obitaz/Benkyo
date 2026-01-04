3d0d233f5ebd80d2e1df2225b30a3b4b8840a132
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-01-04 20:07:57 +0800

    Client Authentication Services

---
+++ Benkyo.Client/Services/AuthService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using Shared.Models;
+namespace Benkyo.Client.Services
+{
+    public class AuthService
+    {
+        private readonly HttpClient _httpClient;
+
+        public AuthService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+
+        }
+
+        public async Task<LoginModel> LoginAsync(string email, string password)
+        {
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync("api/auth/login", new LoginModel {  Email = email, Password = password});
+
+                var jsonContent = await response.Content.ReadAsStringAsync();
+
+                var loginResponse = JsonSerializer.Deserialize<LoginModel>(jsonContent, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                if(response.IsSuccessStatusCode && loginResponse != null)
+                {
+                    Console.WriteLine("True Here");
+                    return loginResponse;
+                }
+                return new LoginModel();
+
+            
+            }catch(Exception ex)
+            {
+                throw new Exception("An error occurred during login.", ex);
+            }
+        }
+    }
+}
