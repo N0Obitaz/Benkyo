2c31a4c0ba19b35bb647b8181966256cec5cb276
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-01-04 20:08:34 +0800

    Firebase Authentication Config with non-hardcoded ApiKey credentials

---
+++ Benkyo/Services/FirebaseAuthentication.cs
@@ -0,0 +1,39 @@
+﻿using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+
+namespace Benkyo.Services
+{
+    public class FirebaseAuthentication
+    {
+
+        public FirebaseAuthConfig Config { get;  }
+        
+
+        public FirebaseAuthentication(IConfiguration firebaseCredential)
+        {
+
+            try
+            {
+                var apiKey = firebaseCredential["Firebase:ApiKey"];
+                var authDomain = firebaseCredential["Firebase:AuthDomain"];
+                Config = new FirebaseAuthConfig
+                {
+                    ApiKey = apiKey,
+                    AuthDomain = authDomain,
+                    Providers = new FirebaseAuthProvider[]
+                    {
+                        new EmailProvider()
+                    },
+                    UserRepository = new FileUserRepository("benkyo")
+                   
+                };
+            } catch(Exception ex)
+            
+            {
+                throw new Exception("An error occurred while initializing Firebase Authentication.", ex);
+            }
+
+        }
+    }
+}
