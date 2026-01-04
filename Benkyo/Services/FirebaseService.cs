0393a88fae57630137eef4b09d3d6a133d3a0de7
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-01-04 20:04:24 +0800

    Firebase Admin sdk working

--- Benkyo/Services/FirebaseService.cs
+++ Benkyo/Services/FirebaseService.cs
@@ -1,4 +1,7 @@
-﻿using Google.Cloud.Firestore;
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
 
 namespace Benkyo.Services
 {
@@ -6,10 +9,24 @@ namespace Benkyo.Services
     {
 
         private readonly FirestoreDb _db;
+        private readonly FirebaseApp _firebaseApp;
 
-        public FirebaseService(FirestoreDb db)
+        public FirebaseService(FirestoreDb db, FirebaseApp firebaseApp)
         {
             _db = db;
+            _firebaseApp = firebaseApp;
+        }
+
+        public async Task<UserRecord> GetUserByEmailAsync(string email)
+        {
+            try
+            {
+                return await FirebaseAuth.DefaultInstance.GetUserByEmailAsync(email);
+
+            } catch
+            {
+                return null;
+            }
         }
 
     }
