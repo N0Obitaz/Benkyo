b6d5ac3711ccb0ce2dea710bdc78598296cd8bfe
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-01-11 23:08:21 +0800

    Added Lesson controller

---
+++ Benkyo/Controllers/LessonController.cs
@@ -0,0 +1,46 @@
+﻿using Benkyo.Services;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+
+namespace Benkyo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LessonController : ControllerBase
+    {
+        private readonly FirebaseService _firebaseService;
+
+        public LessonController(FirebaseService firebaseService)
+        {
+            _firebaseService = firebaseService;
+        }
+
+        [HttpPost("createlesson")]
+        private async Task<IActionResult> CreateLesson([FromBody] Lesson lessonRequest)
+        {
+            try
+            {
+                var existing = await _firebaseService._db.Collection("lessons").GetSnapshotAsync();
+
+                if (!(existing == null)) return BadRequest(new { Message = "That lesson already exists" });
+                {
+                    var lessonRef = _firebaseService._db.Collection("lessons").Document();
+                    var lessonData = new Dictionary<string, object>
+                    {
+                        { "lessonName", lessonRequest.LessonName ?? "Untitled Lesson" },
+                        { "flashcards", lessonRequest.Flashcards ?? new List<Flashcard>() }
+                    };
+                    await lessonRef.SetAsync(lessonData);
+
+                    return Ok(new { Message = "Lesson Created" });
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error creating lesson", ex);
+            }
+        }
+    }
+}
