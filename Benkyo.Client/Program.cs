527825d00e6ae18ece9284cc701bc17c7b6452c7
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-01-27 22:30:05 +0800

    feat: registered AddMemoryCache on the client

--- Benkyo.Client/Program.cs
+++ Benkyo.Client/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddScoped(sp => new HttpClient
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
+builder.Services.AddMemoryCache();
+
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<StudysetService>();
 builder.Services.AddScoped<Studyset>();
