dcb43257519617ce0248caa5a096840a3fb18308
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-01-30 23:27:38 +0800

    feat: implemented fetching of studysets partially

---
+++ Benkyo/Controllers/PublicStudysetController.cs
@@ -0,0 +1,31 @@
+﻿using Benkyo.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Benkyo.Controllers
+{
+    [ApiController]
+    [Route("api[controller]")]
+    public class PublicStudysetController : ControllerBase
+    {
+        private readonly FirebaseService _firebaseService;
+        private readonly IMemoryCache _memoryCache;
+
+        public PublicStudysetController(FirebaseService firebaseService, IMemoryCache memoryCache)
+        {
+            _firebaseService = firebaseService;
+            _memoryCache = memoryCache;
+        }
+
+        //Fetch 10 rows of studysets only
+        [HttpGet]
+        public async Task<IActionResult> FetchPublicStudysets()
+        {
+            var studysetRef = _firebaseService._db.Collection("studysets");
+
+            var snapshot = await studysetRef.GetSnapshotAsync();
+            return Ok();
+        }
+
+    }
+}
