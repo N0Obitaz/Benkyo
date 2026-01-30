25237ff2ec2bdbf05987d716a940985c89e74e15
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-01-30 23:20:02 +0800

    feat: added api controller related to public studysets

---
+++ Benkyo/Controllers/PublicFlashcardController.cs
@@ -0,0 +1,17 @@
+﻿using Benkyo.Services;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Benkyo.Controllers
+{
+    public class PublicFlashcardController
+    {
+        private readonly FirebaseService _firebaseService;
+        private readonly IMemoryCache _memoryCache;
+
+        public PublicFlashcardController(FirebaseService firebaseService, IMemoryCache memoryCache)
+        {
+            _firebaseService = firebaseService;
+            _memoryCache = memoryCache;
+        }
+    }
+}
