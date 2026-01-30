c7e94fa8c83da38b2f58777f1a2cec8147f30a13
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-01-30 23:39:16 +0800

    feat: implemented IMemoryCaching for now

--- Benkyo.Client/Services/PublicStudysetService.cs
+++ Benkyo.Client/Services/PublicStudysetService.cs
@@ -1,7 +1,15 @@
-﻿namespace Benkyo.Client.Services
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace Benkyo.Client.Services
 {
     public class PublicStudysetService
     {
         private readonly HttpClient ht = new HttpClient { BaseAddress = new Uri("localhost://7218") };
+
+        private readonly IMemoryCache _memoryCache;
+        public StudysetService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
     }
 }
