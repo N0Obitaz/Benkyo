c80d8db16a17f70fdde131714da8fa8c2d7fc653
N0Obitaz <charlesbernard.balaguer.041504@gmail.com>
2026-02-04 23:24:51 +0800

    feat: added async function for issuing authentication for user

--- Benkyo/Services/AuthenticationService.cs
+++ Benkyo/Services/AuthenticationService.cs
@@ -1,6 +1,22 @@
-﻿namespace Benkyo.Services
+﻿using Shared.Models;
+
+namespace Benkyo.Services
 {
     public class AuthenticationService
     {
+
+        public async Task AuthenticateUser(User user)
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error authenticating user: {ex.Message}");)
+            }
+        }
+
     }
 }
